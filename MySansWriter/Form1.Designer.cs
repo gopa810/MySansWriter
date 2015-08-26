7b5ef1e52ee59d7779741fcc8dbafc59c1af9d84
gopa810 <root@gopal.home.sk>
2015-08-26 13:41:06 +0200

    v1

---
+++ MySansWriter/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace MySansWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(488, 186);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            this.richTextBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.richTextBox1_PreviewKeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(440, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MySansWriter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MySansWriter/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MySansWriter
+{
+    public partial class Form1 : Form
+    {
+        public class RH
+        {
+            public Keys key;
+            public bool control;
+            public bool alt;
+            public string text;
+            public RH()
+            {
+            }
+
+            public RH(Keys k, bool c, bool a, string t)
+            {
+                key = k;
+                control = c;
+                alt = a;
+                text = t;
+            }
+        };
+
+        public RH[] map = new RH[]{
+            new RH(Keys.A, true, false, "ā"),
+            
+            new RH(Keys.D, false, true, "ḍ"),
+            
+            new RH(Keys.H, false, true, "ḥ"),
+            
+            new RH(Keys.I, true, false, "ī"),
+            
+            new RH(Keys.L, false, true, "ḷ"),
+            new RH(Keys.L, true, false, "ḹ"),
+            
+            new RH(Keys.M, true, false, "ṁ"),
+            new RH(Keys.M, false, true, "ṃ"),
+            
+            new RH(Keys.N, true, false, "ṅ"),
+            new RH(Keys.N, false, true, "ṇ"),
+            new RH(Keys.N, true, true, "ñ"),
+
+            new RH(Keys.R, true, false, "ṝ"),
+            new RH(Keys.R, false, true,"ṛ"),
+            
+            new RH(Keys.S, false, true, "ṣ"),
+            new RH(Keys.S, true, false, "ś"),
+            
+            new RH(Keys.T, false, true, "ṭ"),
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C && e.Alt == false && e.Control == true)
+            {
+                e.Handled = true;
+                int start = richTextBox1.SelectionStart;
+                int length = richTextBox1.SelectionLength;
+                richTextBox1.SelectAll();
+                richTextBox1.Copy();
+                richTextBox1.SelectionStart = start;
+                richTextBox1.SelectionLength = length;
+                return;
+            }
+
+            //Debugger.Log(0, "", "KeyDown: " + e.KeyCode.ToString() + ", C: " + e.Control + ", A: " + e.Alt + "\n");
+            foreach (RH rh in map)
+            {
+                //Debugger.Log(0, "", "  TEST: " + rh.key.ToString() + ", C: " + rh.control + ", A: " + rh.alt + "\n");
+                if (rh.key == e.KeyCode && rh.alt == e.Alt && rh.control == e.Control)
+                {
+                    //Debugger.Log(0, "", "    HIT: " + rh.text + "\n");
+                    e.Handled = true;
+                    richTextBox1.SelectedText = rh.text;
+                    break;
+                }
+            }
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Debugger.Log(0, "", "KeyUp: " + e.KeyCode.ToString() + "\n");
+        }
+    }
+}
